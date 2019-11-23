5bc592afe677daef0d5120388b4330d5ae4a5978
Dmitriy Pashkevich <dmitriytemn@gmail.com>
2019-11-23 18:38:01 +0300

    Added input component

---
+++ src/components/TextInput/TextInput.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import cn from 'classnames';
+import cm from './TextInput.module.css';
+
+export const TextInput = ({className, onChange, ...rest}) => (
+    <input className={cn(cm.textInput, className)} onChange={onChange} {...rest} />
+);
+
+TextInput.propTypes = {
+    onChange: PropTypes.func,
+    className: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.object,
+    ]),
+};
---
+++ src/components/TextInput/__tests__/TextInput.test.js
@@ -0,0 +1,23 @@
+import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
+
+import React from 'react';
+import {fireEvent, render} from '@testing-library/react';
+
+import {TextInput} from '../TextInput';
+
+describe('TextInput', () => {
+    it('Should be render and change value', () => {
+        let inputValue = '';
+
+        const {container} = render(
+            <TextInput onChange={e => inputValue = e.target.value}  />
+        );
+
+        const inputComponent = container.firstChild;
+
+        fireEvent.change(inputComponent, {target: {value: 'react'}});
+        expect(inputComponent.value).toBe('react');
+        expect(inputValue).toBe('react');
+    });
+});
