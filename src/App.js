25f0f32590d4d85de6947699214cfef4371af37a
Dmitriy Pashkevich <dmitriytemn@gmail.com>
2019-11-24 17:02:48 +0300

    Added checking online/offline mode after app loaded

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 
+import useOnlineStatus from '@rehooks/online-status';
+
 import { ApolloProvider } from '@apollo/react-hooks';
 
 import { gqlClient } from './gql/client';
 import { GithubApp } from './app/GithubApp';
+import { Error } from 'components/Error';
 
 function App() {
-  return (
+  const onlineStatus = useOnlineStatus();
+
+  return onlineStatus ? (
     <ApolloProvider client={gqlClient}>
       <GithubApp />
     </ApolloProvider>
+  ) : (
+    <Error text="No network connection. Please reload page or enable network connection." />
   );
 }
 
