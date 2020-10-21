659d0efaa3d29d2d905b42e55dc75783729d45e0
Dmitriy Pashkevich <dmitriytemn@gmail.com>
2020-10-21 21:46:31 +0500

    Added request personal-access-token if not exist in localstorage or env.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { ApolloProvider } from '@apollo/client';
 
@@ -8,8 +8,29 @@ import { GithubApp } from './app/GithubApp';
 import { OnlineStatus } from 'components/OnlineStatus';
 
 function App() {
+  const isTokenExist = Boolean(process.env.REACT_APP_GITHUB_TOKEN || localStorage.getItem('token'));
+
+  const updateToken = () => {
+    const token = prompt('Введите ваш personal-access-token от Github, чтобы начать взаимодействие с API');
+
+    localStorage.setItem('token', token);
+    window.location = '/';
+  };
+
+  useEffect(() => {
+    if (!isTokenExist) {
+      updateToken();
+    }
+  }, [isTokenExist]);
+
   return (
     <OnlineStatus>
+      <div style={{ padding: '15px 15px 0' }}>
+        Если вы введете неверный personal-access-token, то получите 401 ошибку, данные не будут загружены.
+        <br />
+        <br />
+        <button onClick={updateToken}>Ввести personal-access-token повторно</button>
+      </div>
       <ApolloProvider client={gqlClient}>
         <GithubApp />
       </ApolloProvider>
