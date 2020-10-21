6eec122129c969be0cdcb27f7da52717e7d11883
Dmitriy Pashkevich <dmitriytemn@gmail.com>
2020-10-21 22:17:44 +0500

    Fixed prompt for gh-pages

--- src/gql/client.js
+++ src/gql/client.js
@@ -7,6 +7,7 @@ const httpLink = createHttpLink({
 
 const authLink = setContext((_, { headers }) => {
   const token = process.env.REACT_APP_GITHUB_TOKEN || localStorage.getItem('token');
+  console.log(token)
 
   return {
     headers: {
