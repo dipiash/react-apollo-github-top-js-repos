a8fc03ffbc55212377c3dd353cbaec0e5d6efcd4
Dmitriy Pashkevich <dmitriytemn@gmail.com>
2020-10-21 22:01:29 +0500

    Updates

---
+++ precache-manifest.679f47cdbeb39745d6336d62a155bf4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fc82e6130aa1dec2fc2571bfa4404b4",
+    "url": "/react-apollo-github-top-js-repos/index.html"
+  },
+  {
+    "revision": "479809d39ec19b5c7025",
+    "url": "/react-apollo-github-top-js-repos/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "b4763ce88f1ea6eaec77",
+    "url": "/react-apollo-github-top-js-repos/static/css/main.a3e60bb0.chunk.css"
+  },
+  {
+    "revision": "479809d39ec19b5c7025",
+    "url": "/react-apollo-github-top-js-repos/static/js/2.be9bf28a.chunk.js"
+  },
+  {
+    "revision": "220ce693b6dfa473e6e0156ac2ec8d5d",
+    "url": "/react-apollo-github-top-js-repos/static/js/2.be9bf28a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4763ce88f1ea6eaec77",
+    "url": "/react-apollo-github-top-js-repos/static/js/main.78251cce.chunk.js"
+  },
+  {
+    "revision": "7e9561b89040494ec374",
+    "url": "/react-apollo-github-top-js-repos/static/js/runtime-main.08b75595.js"
+  }
+]);
